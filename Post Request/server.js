c15ed1228960b05563bc8c7aeac7d0c5e3f3ad0a
yudigarg3043 <yudishgarg645@gmail.com>
2025-02-24 17:09:34 +0530

    Saving Form Data from URLEncoded to object

--- Post Request/server.js
+++ Post Request/server.js
@@ -3,11 +3,33 @@
 
 const express = require("express");
 const app = express();
+//It is middleware that converts the urlencoded form data to object that is readable by server.js.
+app.use(express.urlencoded({extended: true}));
 
-app.get('/signup', (req, res) => {
+
+let userData = [];
+
+app.get('/adduser', (req, res) => {
     res.sendFile(__dirname + '/signup.html');
 });
 
+app.post("/adduser", (req, res) => {
+    let {name, email, password} = req.body;
+    console.log(`Name: ${name}, Email: ${email}, Password: ${password}`);
+    let newUser = {
+        name: name,
+        email: email,
+        password: password
+    };
+    userData.push(newUser);
+    // for(let i=0; i<userData.length; i++){
+    //     if(userData[i].name == newUser.name && userData[i].email == newUser.email && userData[i].password == newUser.password){
+    //         res.send("User already exists");
+    //         return;
+    //     }
+    // }
+    res.send(userData);
+});
 
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
