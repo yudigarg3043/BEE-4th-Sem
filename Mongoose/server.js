7003774a42a48d09ccaa8764d60018355ee9c35f
yudigarg3043 <yudishgarg645@gmail.com>
2025-03-26 16:57:21 +0530

    Reading and showing data

--- Mongoose/server.js
+++ Mongoose/server.js
@@ -3,7 +3,34 @@ const app = express();
 const mongoose = require("mongoose");
 const port = 3500;
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
+const User = require("./model/user");
+
+//add new user
+app.post("/users", async(req, res) => {
+    let {name,user,email,password} = req.body;
+    let newUser = new User({
+      name: name,
+      email: email,
+      password: password
+    });
+    await newUser.save()
+    res.send("User created");
+});
+
+//get all users
+app.get("/users", async(req, res) => {
+    let alluser = await User.find();
+    res.send(alluser);
+});
+
+//get user by id
+app.get("/users/:id", async(req, res) => {
+    let {id} = req.params.id;
+    let user = await User.findOne(id);
+    res.send(user);
+} );
+
+mongoose.connect('mongodb://127.0.0.1:27017/blogging')
   .then(() => console.log('Connected!'));
 
 app.listen(port, () => {    
