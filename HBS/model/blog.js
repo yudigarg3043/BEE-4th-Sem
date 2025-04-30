17828e87d2b4755a18c42bf6a50b083339840e99
yudigarg3043 <yudishgarg645@gmail.com>
2025-04-30 18:31:58 +0530

    Further Proceding with HBS

--- HBS/model/blog.js
+++ HBS/model/blog.js
@@ -1,25 +1,11 @@
 const mongoose = require("mongoose");
 
-// let blogSchema = new mongoose.Schema({
-//     title : {
-//         type : String,
-//         require : true
-//     },
-//     content : {
-//         type : String,
-//         require : true
-//     },
-//     author : {
-//         type : String,
-//         require : true
-//     },
-//     date : {
-//         type : Date,
-//         default : Date.now()
-//     }
-// });
 let blogSchema = new mongoose.Schema({
     Title : {
+        type : String,
+        require : true
+    },
+    Content : {
         type : String,
         require : true
     },
@@ -27,6 +13,20 @@ let blogSchema = new mongoose.Schema({
         type : String,
         require : true
     }
+    // date : {
+    //     type : Date,
+    //     default : Date.now()
+    // }
 });
+// let blogSchema = new mongoose.Schema({
+//     Title : {
+//         type : String,
+//         require : true
+//     },
+//     Writer : {
+//         type : String,
+//         require : true
+//     }
+// });
 
 module.exports = mongoose.model("Blog", blogSchema);
--- HBS/server.js
+++ HBS/server.js
@@ -8,6 +8,7 @@ app.set('view engine', 'hbs');
 // const userRoutes = require("./routes/userRoutes");
 
 // const blogRoutes = require("./routes/blogRoutes");
+const user = require("./model/user");
 const blog = require("./model/blog");
 
 app.use(express.json());
@@ -32,7 +33,30 @@ app.get("/blogs", async(req,res)=>{
     res.render("blogs",{
         data : all
     });
-});
+})
+
+app.get("/alluser", async(req,res)=>{
+    let all = await user.find();
+    res.render("alluser",{
+        data : all
+    })
+})
+app.get("/alluser/:name", async(req,res)=>{
+    let name = req.params.name;
+    let all = await user.findOne({name:name});
+    res.render("alluser",{
+        data : all
+    })
+})
+
+app.get("/blogs/:id", async(req,res)=>{
+    let {id} = req.params;
+    let blogs = await blog.findById(id);
+
+    res.render("blog",{
+        blog: blogs
+    })
+})
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/blogging').then(() => console.log('Mongodb Connected!'));
